3f06cf1710fee38d95562bf315101e77995077c2
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-07-20 01:15:27 +0700

    core: add deeplinking for help menu
    Signed-off-by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -307,7 +307,17 @@ def get_help(update, context):
 
     # ONLY send help in PM
     if chat.type != chat.PRIVATE:
-
+        if chat.type != chat.PRIVATE:
+        if len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
+            module = args[1].lower()
+            update.effective_message.reply_text(
+            f"Contact me in PM to get help of {module.capitalize()}",
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton(
+                    text="Help",
+                    url="t.me/{}?start=ghelp_{}".format(context.bot.username, module))
+            ]]))
+            return
         update.effective_message.reply_text(
             "Click the button below to get help menu in your pm.",
             reply_markup=InlineKeyboardMarkup(
