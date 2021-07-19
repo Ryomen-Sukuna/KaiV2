f3e8da3f8996f74f349b891df372feafb801a092
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-07-19 21:29:23 +0700

    misc

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -1,10 +1,8 @@
-from math import ceil
-from typing import List, Dict
-
-from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode
+from typing import Dict, List
+from uuid import uuid4
+from tg_bot import NO_LOAD
+from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, InlineQueryResultArticle, InputTextMessageContent
 from telegram.error import TelegramError
-
-from tg_bot import LOAD, NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -21,19 +19,22 @@ class EqInlineKeyboardButton(InlineKeyboardButton):
 def split_message(msg: str) -> List[str]:
     if len(msg) < MAX_MESSAGE_LENGTH:
         return [msg]
-    lines = msg.splitlines(True)
-    small_msg = ""
-    result = []
-    for line in lines:
-        if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
-            small_msg += line
-        else:
-            result.append(small_msg)
-            small_msg = line
-    # Else statement at the end of the for loop, so append the leftover string.
-    result.append(small_msg)
 
-    return result
+    else:
+        lines = msg.splitlines(True)
+        small_msg = ""
+        result = []
+        for line in lines:
+            if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
+                small_msg += line
+            else:
+                result.append(small_msg)
+                small_msg = line
+        else:
+            # Else statement at the end of the for loop, so append the leftover string.
+            result.append(small_msg)
+
+        return result
 
 
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
@@ -62,29 +63,37 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             ]
         )
 
-    pairs = list(zip(modules[::2], modules[1::2]))
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
 
-    if len(modules) % 2 == 1:
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc == 1:
         pairs.append((modules[-1],))
-
-    max_num_pages = ceil(len(pairs) / 7)
-    modulo_page = page_n % max_num_pages
-
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 7:
-        pairs = pairs[modulo_page * 7 : 7 * (modulo_page + 1)] + [
-            (
-                EqInlineKeyboardButton(
-                    "<", callback_data="{}_prev({})".format(prefix, modulo_page)
-                ),
-                EqInlineKeyboardButton(
-                    ">", callback_data="{}_next({})".format(prefix, modulo_page)
-                ),
-            )
-        ]
+    elif calc == 2:
+        pairs.append((modules[-1],))
 
     return pairs
 
+def article(
+    title: str = "",
+    description: str = "",
+    message_text: str = "",
+    thumb_url: str = None,
+    reply_markup: InlineKeyboardMarkup = None,
+    disable_web_page_preview: bool = False,
+) -> InlineQueryResultArticle:
+
+    return InlineQueryResultArticle(
+        id=uuid4(),
+        title=title,
+        description=description,
+        thumb_url=thumb_url,
+        input_message_content=InputTextMessageContent(
+            message_text=message_text,
+            disable_web_page_preview=disable_web_page_preview,
+        ),
+        reply_markup=reply_markup,
+    )
 
 def send_to_list(
     bot: Bot, send_to: list, message: str, markdown=False, html=False
@@ -125,5 +134,18 @@ def revert_buttons(buttons):
     return res
 
 
+def build_keyboard_parser(bot, chat_id, buttons):
+    keyb = []
+    for btn in buttons:
+        if btn.url == "{rules}":
+            btn.url = "http://t.me/{}?start={}".format(bot.username, chat_id)
+        if btn.same_line and keyb:
+            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
+        else:
+            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
+
+    return keyb
+
+
 def is_module_loaded(name):
-    return (not LOAD or name in LOAD) and name not in NO_LOAD
+    return name not in NO_LOAD
