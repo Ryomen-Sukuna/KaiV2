cda06f28230ef5d5ff1e75853c03f167df0ef275
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-07-19 15:00:50 +0700

    move fun parts from misc to meme
    Signed-off-by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

---
+++ tg_bot/modules/memes.py
@@ -0,0 +1,55 @@
+import random
+from typing import Optional, List 
+
+from tg_bot import dispatcher
+import tg_bot.modules.helper_funcs.meme_strings as meme
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext
+
+
+def runs(update: Update, context: CallbackContext):
+    temp = random.choice(meme.RUN_STRINGS)
+    if update.effective_user.id == 1170714920:
+        temp = "Run everyone, they just dropped a bomb 💣💣"
+    update.effective_message.reply_text(temp)
+
+def slap(update: Update, args: List[str]):
+    msg = update.effective_message  # type: Optional[Message]
+
+    # reply to correct message
+    reply_text = (
+        msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
+    )
+
+    # get user who sent message
+    if msg.from_user.username:
+        curr_user = "@" + escape_markdown(msg.from_user.username)
+    else:
+        curr_user = "[{}](tg://user?id={})".format(
+            msg.from_user.first_name, msg.from_user.id
+        )
+
+    user_id = extract_user(update.effective_message, args)
+    if user_id:
+        slapped_user = bot.get_chat(user_id)
+        user1 = curr_user
+        if slapped_user.username:
+            user2 = "@" + escape_markdown(slapped_user.username)
+        else:
+            user2 = "[{}](tg://user?id={})".format(
+                slapped_user.first_name, slapped_user.id
+            )
+
+    # if no target found, bot targets the sender
+    else:
+        user1 = "[{}](tg://user?id={})".format(bot.first_name, bot.id)
+        user2 = curr_user
+
+    temp = random.choice(meme.SLAP_TEMPLATES)
+    item = random.choice(meme.ITEMS)
+    hit = random.choice(meme.HIT)
+    throw = random.choice(meme.THROW)
+
+    repl = temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw)
+
+    reply_text(repl, parse_mode=ParseMode.MARKDOWN)
