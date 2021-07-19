f7ab04bd49f6238c8bea721d00ec99cfa9ce75fd
Ryomen-Sukuna <ryomensukuna83@gmail.com>
2021-07-19 13:01:27 +0700

    notes: fixes typo
    Signed-off-by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- tg_bot/modules/notes.py
+++ tg_bot/modules/notes.py
@@ -543,11 +543,9 @@ __mod_name__ = "Notes"
 GET_HANDLER = CommandHandler("get", cmd_get, run_async=True)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get, run_async=True)
 SLASH_GET_HANDLER = MessageHandler(Filters.regex(r"^/\d+$"), slash_get, run_async=True)
-SAVE_HANDLER = CommandHandler("save", save)
-DELETE_HANDLER = CommandHandler("clear", clear), z, run_async=Truee
-
+SAVE_HANDLER = CommandHandler("save", save, run_async=True)
+DELETE_HANDLER = CommandHandler("clear", clear, run_async=True)
 LIST_HANDLER = DisableAbleCommandHandler(["notes", "saved"], list_notes, admin_ok=True, run_async=True)
-
 CLEARALL = DisableAbleCommandHandler("removeallnotes", clearall, run_async=True)
 CLEARALL_BTN = CallbackQueryHandler(clearall_btn, pattern=r"notes_.*", run_async=True)
 
