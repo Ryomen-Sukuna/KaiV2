22366f7c742bef75d258e9f72b617cb711718b9e
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-07-19 19:23:14 +0700

    init: ->
    init: made an init class for parsing config file and storing variables.
    
    Credits to EnterpriseALRobot 
    <https://github.com/Dank-del/EnterpriseALRobot>

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -1,144 +1,142 @@
 import logging
 import os
 import sys
+import time
 import spamwatch
-
 import telegram.ext as tg
+from configparser import ConfigParser
+from logging.config import fileConfig
+
+StartTime = time.time()
+
+def get_user_list(key):
+    # Import here to evade a circular import
+    from tg_bot.modules.sql import nation_sql
+    super = super_sql.get_levels(key)
+    return [a.user_id for a in levels]
 
 # enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-)
 
-LOGGER = logging.getLogger(__name__)
+fileConfig('logging.ini')
+
+log = logging.getLogger('[IronBlooD]')
+logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(logging.WARNING)
+log.info("[KAI] KAI is starting. | An Iron Blood Project. | Licensed under GPLv3.")
+log.info("[KAI] Not affiliated to Villain or Yostar in any way whatsoever.")
+log.info("[KAI] Project maintained by: github.com/Ryomen-Sukuna (t.me/Anomaliii)")
 
 # if version < 3.6, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+if sys.version_info[0] < 3 or sys.version_info[1] < 7:
+    log.error(
+        "[KAI] You MUST have a python version of at least 3.7! Multiple features depend on this. Bot quitting."
     )
-    sys.exit(1)
+    quit(1)
 
-ENV = bool(os.environ.get("ENV", False))
+parser = ConfigParser()
+parser.read("config.ini")
+kaiconfig = parser["kaiconfig"]
 
-if ENV:
-    TOKEN = os.environ.get("TOKEN", None)
-
-    try:
-        SUDO_USERS = (int(x) for x in os.environ.get("SUDO_USERS", "").split())
-        DEV_USERS = (int(x) for x in os.environ.get("DEV_USERS", "").split())
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
-
-    MESSAGE_DUMP = os.environ.get("MESSAGE_DUMP", None)
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
-
-    try:
-        SUPPORT_USERS = (int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        WHITELIST_USERS = (
-            int(x) for x in os.environ.get("WHITELIST_USERS", "").split()
-        )
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
-    URL = os.environ.get("URL", "")  # Does not contain token
-    PORT = int(os.environ.get("PORT", 5000))
-    CERT_PATH = os.environ.get("CERT_PATH")
-
-    DB_URI = os.environ.get("DATABASE_URL")
-    DONATION_LINK = os.environ.get("DONATION_LINK")
-    LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
-    WORKERS = int(os.environ.get("WORKERS", 8))
-    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
-    CF_API_KEY = os.environ.get("CF_API_KEY", None)
-
-else:
-    from tg_bot.config import Development as Config
-
-    TOKEN = Config.TOKEN
-
-    try:
-        OWNER_ID = int(Config.OWNER_ID)
-    except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
-
-    MESSAGE_DUMP = Config.MESSAGE_DUMP
-    OWNER_USERNAME = Config.OWNER_USERNAME
-
-    try:
-        SUDO_USERS = (int(x) for x in Config.SUDO_USERS or [])
-        DEV_USERS = (int(x) for x in Config.DEV_USERS or [])
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
-
-    try:
-        SUPPORT_USERS = (int(x) for x in Config.SUPPORT_USERS or [])
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        WHITELIST_USERS = (int(x) for x in Config.WHITELIST_USERS or [])
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    WEBHOOK = Config.WEBHOOK
-    URL = Config.URL
-    PORT = Config.PORT
-    CERT_PATH = Config.CERT_PATH
-
-    DB_URI = Config.SQLALCHEMY_DATABASE_URI
-    DONATION_LINK = Config.DONATION_LINK
-    LOAD = Config.LOAD
-    NO_LOAD = Config.NO_LOAD
-    DEL_CMDS = Config.DEL_CMDS
-    STRICT_GBAN = Config.STRICT_GBAN
-    WORKERS = Config.WORKERS
-    BAN_STICKER = Config.BAN_STICKER
-    ALLOW_EXCL = Config.ALLOW_EXCL
-    SPAMWATCH_API = Config.SPAMWATCH_API
-    CF_API_KEY = Config.CF_API_KEY
+class KaiINIT:
+    def __init__(self, parser):
+        self.parser = parser
+        self.SYS_ADMIN = self.parser.getint('SYS_ADMIN', 0)
+        self.OWNER_ID = self.parser.getint('OWNER_ID')
+        self.OWNER_USERNAME = self.parser.get('OWNER_USERNAME', None)
+        self.API_ID = self.parser.getint("API_ID")
+        self.API_HASH = self.parser.get("API_HASH")
+        self.WEBHOOK = self.parser.getboolean('WEBHOOK', False)
+        self.URL = self.parser.get('URL', None)
+        self.CERT_PATH = self.parser.get('CERT_PATH', None)
+        self.PORT = self.parser.getint('PORT', None)
+        self.INFOPIC = self.parser.getboolean('INFOPIC', False)
+        self.DEL_CMDS = self.parser.getboolean("DEL_CMDS", False)
+        self.STRICT_GBAN = self.parser.getboolean("STRICT_GBAN", False)
+        self.ALLOW_EXCL = self.parser.getboolean("ALLOW_EXCL", False)
+        self.CUSTOM_CMD = ['/', '!']
+        self.BAN_STICKER = self.parser.get("BAN_STICKER", None)
+        self.TOKEN = self.parser.get("TOKEN")
+        self.DB_URI = self.parser.get("SQLALCHEMY_DATABASE_URI")
+        self.LOAD = self.parser.get("LOAD").split()
+        self.LOAD = list(map(str, self.LOAD))
+        self.MESSAGE_DUMP = self.parser.getint('MESSAGE_DUMP', None)
+        self.GBAN_LOGS = self.parser.getint('GBAN_LOGS', None)
+        self.NO_LOAD = self.parser.get("NO_LOAD").split()
+        self.NO_LOAD = list(map(str, self.NO_LOAD))
+        self.spamwatch_api = self.parser.get('spamwatch_api', None)
+        self.CF_API_KEY =  self.parser.get("CF_API_KEY", None)
+        self.bot_id = 0 #placeholder
+        self.bot_name = "Kai" #placeholder
+        self.bot_username = "KaiRobot" #placeholder
 
 
-SUDO_USERS.append(OWNER_ID)
-DEV_USERS.append(OWNER_ID)
+    def init_sw(self):
+        if self.spamwatch_api is None:
+            log.warning("SpamWatch API key is missing! Check your config.ini")
+            return None
+        else:
+            try:
+                sw = spamwatch.Client(spamwatch_api)
+                return sw
+            except:
+                sw = None
+                log.warning("Can't connect to SpamWatch!")
+                return sw
 
-if not SPAMWATCH_API:
-    sw = None
-    LOGGER.warning("SpamWatch API key missing! recheck your config.")
-else:
-    try:
-        sw = spamwatch.Client(SPAMWATCH_API)
-    except:
-        sw = None
-        LOGGER.warning("Can't connect to SpamWatch!")
 
-updater = tg.Updater(TOKEN, workers=WORKERS)
+KInit = KaiINIT(parser=kigconfig)
+
+SYS_ADMIN = KInit.SYS_ADMIN
+OWNER_ID = KInit.OWNER_ID
+OWNER_USERNAME = KInit.OWNER_USERNAME
+API_ID = KInit.API_ID
+API_HASH = KInit.API_HASH
+WEBHOOK = KInit.WEBHOOK
+URL = KInit.URL
+CERT_PATH = KInit.CERT_PATH
+PORT = KInit.PORT
+INFOPIC = KInit.INFOPIC
+DEL_CMDS = KInit.DEL_CMDS
+ALLOW_EXCL = KInit.ALLOW_EXCL
+CUSTOM_CMD = KInit.CUSTOM_CMD
+BAN_STICKER = KInit.BAN_STICKER
+TOKEN = KInit.TOKEN
+DB_URI = KInit.DB_URI
+LOAD = KInit.LOAD
+MESSAGE_DUMP = KInit.MESSAGE_DUMP
+GBAN_LOGS = KInit.GBAN_LOGS
+NO_LOAD = KInit.NO_LOAD
+SUDO_USERS = [OWNER_ID] + get_user_list("sudos")
+DEV_USERS = [OWNER_ID] + get_user_list("devs")
+SUPPORT_USERS = get_user_list("supports")
+WHITELIST_USERS = get_user_list("whitelists")
+SPAMMERS = get_user_list("spammers")
+spamwatch_api = KInit.spamwatch_api
+CF_API_KEY = KInit.CF_API_KEY
+
+SPB_MODE = kigconfig.getboolean('SPB_MODE', False)
+
+# SpamWatch
+sw = KInit.init_sw()
+
+from tg_bot.modules.sql import SESSION
+
+
+updater = tg.Updater(TOKEN, workers=min(32, os.cpu_count() + 4), request_kwargs={"read_timeout": 10, "connect_timeout": 10})
 dispatcher = updater.dispatcher
 
-SUDO_USERS = list(SUDO_USERS)
-WHITELIST_USERS = list(WHITELIST_USERS)
-SUPPORT_USERS = list(SUPPORT_USERS)
+
 
 # Load at end to ensure all prev variables have been set
-from tg_bot.modules.helper_funcs.handlers import (
-    CustomCommandHandler,
-    CustomRegexHandler,
-)
+from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler
 
-# make sure the regex handler can take extra kwargs
-tg.RegexHandler = CustomRegexHandler
-
-if ALLOW_EXCL:
+if CUSTOM_CMD and len(CUSTOM_CMD) >= 1:
     tg.CommandHandler = CustomCommandHandler
+
+
+def spamfilters(text, user_id, chat_id):
+    # print("{} | {} | {}".format(text, user_id, chat_id))
+    if int(user_id) not in SPAMMERS:
+        return False
+
+    print("This user is a spammer!")
+    return True
