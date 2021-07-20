ac8392970c5a34a8161eb6f278962adf14ca1ef4
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-07-20 11:19:16 +0700

    misc: done
    Signed-off-by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -71,13 +71,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             ]
         )
 
-    pairs = [modules[i * 4 : (i + 1) * 4] for i in range((len(modules) + 4 - 1) // 4)]
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
 
-    round_num = len(modules) / 4
+    round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
-        pairs.append((modules[-1],))
-    elif calc == 2:
         pairs.append((modules[-1],))
 
     else:
