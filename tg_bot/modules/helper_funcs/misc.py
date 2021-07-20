7207fe2f8be3160a7f8e622efd58c11a8e392167
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-07-20 10:11:48 +0700

    misc

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             ]
         )
 
-    pairs = pairs [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)] + 
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)] + 
             (EqInlineKeyboardButton("🔙 Back", callback_data="help_back)]
 
     round_num = len(modules) / 3
